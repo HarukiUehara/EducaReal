ef836fb1ef0d8e94f686487ab1734b79aa60e3f2
unknown <mariaeduardaabreu005@gmail.com>
2025-08-26 21:23:49 -0300

    crete a simple menu

--- app/src/main/java/br/com/fiap/educareal/AppNavigation.kt
+++ app/src/main/java/br/com/fiap/educareal/AppNavigation.kt
@@ -1,6 +1,15 @@
 package br.com.fiap.educareal
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,8 +18,7 @@ import androidx.navigation.compose.composable
 fun AppNavigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "menu") {
         composable("menu") {
-            // Em breve, vamos criar uma tela de menu.
-            // Por enquanto, ela pode redirecionar para uma das outras telas
+            MenuScreen(navController = navController)
         }
         composable("courses") {
             CoursesScreen()
@@ -26,6 +34,48 @@ fun AppNavigation(navController: NavHostController) {
         }
         composable("userProfile") {
             UserProfileScreen()
+        }
+    }
+}
+
+@Composable
+fun MenuScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { navController.navigate("courses") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Cursos")
+        }
+        Button(
+            onClick = { navController.navigate("books") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Livros")
+        }
+        Button(
+            onClick = { navController.navigate("articles") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Artigos")
+        }
+        Button(
+            onClick = { navController.navigate("news") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Notícias")
+        }
+        Button(
+            onClick = { navController.navigate("userProfile") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Perfil do Usuário")
         }
     }
 }
